09fd82abfed381b6f48b1362a20f21dee5e820f9
Artemiy Granat <artemiigranat@gmail.com>
2024-04-10 13:54:11 +0400

    feat: setup postgres database and add initial db schema

---
+++ db/init.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE mm_db;
+\c mm_db;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TYPE user_role AS ENUM ('admin', 'teacher', 'student');
+
+CREATE TABLE users (
+    id uuid DEFAULT uuid_generate_v4(),
+    email varchar(128) UNIQUE NOT NULL,
+    name varchar(32) NOT NULL,
+    surname varchar(32) NOT NULL,
+    patronymic varchar(32),
+    role user_role NOT NULL,
+    password bytea NOT NULL
+);
