c7912a7a51a1e763fe73c808179e7b56b55d9fca
Nikita Rydanov <find.art.in.living@gmail.com>
2024-05-17 15:52:25 +0400

    feat: Additional changes to DB schema
    * Changed organization hierarchical structure modelling
    * Added teacher, student and admin tables
    * Moved constraints into CREATE TABLE bodies
    * Additional small changes (including size of varchars)

--- db/init.sql
+++ db/init.sql
@@ -4,13 +4,34 @@ CREATE DATABASE mm_db;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TYPE user_role AS ENUM ('admin', 'teacher', 'student');
-CREATE TYPE attempt_state AS ENUM ('in_progress', 'completed');
+CREATE TYPE attempt_state AS ENUM ('submitted', 'graded');
 -- NOTE(nrydanov): Need to think of other types together
 CREATE TYPE block_type AS ENUM ('assignment', 'text', 'media', 'quiz');
 
 
+-- NOTE(nrydanov): Hierarchical organization data
+CREATE TABLE organization (
+    id uuid PRIMARY KEY,
+    name varchar(256) NOT NULL
+);
+
+CREATE TABLE faculty (
+    id uuid PRIMARY KEY,
+    name varchar(256) NOT NULL,
+    organization_id uuid NOT NULL REFERENCES organization(id)
+);
+
+CREATE TABLE specialty (
+    id uuid PRIMARY KEY,
+    -- NOTE(nrydanov): Like 03.02.01
+    code varchar(8),
+    name varchar(256) NOT NULL,
+    faculty_id uuid NOT NULL REFERENCES specialty(id)
+);
+
+-- NOTE(nrydanov): General user data
 CREATE TABLE users (
-    id uuid NOT NULL PRIMARY KEY,
+    id uuid PRIMARY KEY,
     -- Auth data
     email varchar(128) UNIQUE NOT NULL,
     password bytea NOT NULL,
@@ -18,24 +39,45 @@ CREATE TABLE users (
     name varchar(32) NOT NULL,
     surname varchar(32) NOT NULL,
     patronymic varchar(32),
-    date_of_birth date,
-    
+    date_of_birth date, 
     -- University data
-    -- NOTE(nrydanov): This data should be enough to determine exact academic group
-    faculty_id uuid NOT NULL,
-    specialty_id uuid NOT NULL,
-    admission_date date NOT NULL,
+    -- NOTE(nrydanov): This data should be enough to determine exact academic groups
     -- Service info
     -- NOTE(nrydanov): Can be used to make admin a teacher for a while
     role_override user_role,
     created_at timestamp NOT NULL
 );
 
+-- NOTE(nrydanov): Specific student data
+CREATE TABLE student (
+    user_id uuid NOT NULL REFERENCES users(id),
+    specialty_id uuid NOT NULL REFERENCES specialty(id),
+    admission_date date NOT NULL,
+    expelled boolean NOT NULL
+);
 
+-- NOTE(nrydanov): Specific teacher data
+CREATE TABLE teacher (
+    user_id uuid NOT NULL REFERENCES users(id),
+    faculty_id uuid NOT NULL REFERENCES faculty(id),
+    promoted_by uuid NOT NULL REFERENCES users(id),
+    promoted_at timestamp NOT NULL
+    -- TODO(nrydanov): Add additional required teacher data if required
+);
+
+-- NOTE(nrydanov): Specific admin data
+CREATE table admin (
+    user_id uuid NOT NULL REFERENCES users(id),
+    promoted_by uuid NOT NULL REFERENCES users(id),
+    promoted_at timestamp NOT NULL
+);
+
+
+-- NOTE(nrydanov): General course data
 CREATE TABLE course (
     id uuid NOT NULL PRIMARY KEY,
-    creator_id uuid NOT NULL,
-    name varchar(64) NOT NULL,
+    creator_id uuid NOT NULL REFERENCES users(id),
+    short_name varchar(64) NOT NULL,
     -- Service info 
     created_at timestamp NOT NULL
 );
@@ -43,28 +85,28 @@ CREATE TABLE course (
 
 -- NOTE(nrydanov): May be helpful to filter out courses for certain faculty
 CREATE TABLE course_faculty (
-    course_id uuid NOT NULL,
-    faculty_id uuid NOT NULL
+    course_id uuid NOT NULL REFERENCES course(id),
+    faculty_id uuid NOT NULL REFERENCES faculty(id)
 );
 
-
--- NOTE(nrydanov): May be helpful to auto-assign user for one of existing
--- group within the course
-CREATE TABLE group_password (
-    password bytea NOT NULL,
-    group_id uuid NOT NULL PRIMARY KEY
-);
-
-
-CREATE TABLE user_group (
+CREATE TABLE groups (
     id uuid NOT NULL PRIMARY KEY,
     name varchar(64) NOT NULL
 );
 
-CREATE TABLE course_user_group (
-    user_id uuid NOT NULL,
-    course_id uuid NOT NULL,
-    group_id uuid NOT NULL
+-- NOTE(nrydanov): May be helpful to auto-assign users for one of existing
+-- groups within the course
+CREATE TABLE groups_password (
+    password bytea NOT NULL,
+    group_id uuid NOT NULL REFERENCES groups(id)
+);
+
+
+
+CREATE TABLE course_users_groups (
+    user_id uuid NOT NULL REFERENCES users(id),
+    course_id uuid NOT NULL REFERENCES course(id),
+    group_id uuid NOT NULL REFERENCES groups(id)
 );
 
 
@@ -77,18 +119,21 @@ CREATE TABLE block (
     data jsonb NOT NULL
 );
 
--- NOTE(nrydanov): group there isn't an academic one, but just multiple tasks
--- on one theme. Maybe there could be a better naming.
-CREATE TABLE task_group (
-    task_group_id uuid NOT NULL,
-    name varchar(64) NOT NULL
+
+CREATE TABLE task_topic (
+    id uuid NOT NULL PRIMARY KEY,
+    title varchar(128) NOT NULL
 );
 
-CREATE TABLE task_group_task (
-    task_group_id uuid NOT NULL,
+
+CREATE TABLE task_topic_task (
+    task_topic_id uuid NOT NULL REFERENCES task_topic(id),
     task_id uuid NOT NULL
 );
 
+CREATE TABLE repository (
+    id uuid NOT NULL PRIMARY KEY
+);
 
 CREATE TABLE task (
     id uuid NOT NULL PRIMARY KEY,
@@ -96,223 +141,73 @@ CREATE TABLE task (
     deadline_at timestamp,
     max_grade real NOT NULL,
     max_attempts integer NOT NULL,
-    lead_time time NOT NULL,
+    lead_time time,
     -- NOTE(nrydanov): Equal to NULL if task doesn't need separate repo
-    repo_id uuid
+    repo_id uuid REFERENCES repository(id)
 );
 
 CREATE TABLE task_git_info (
-    task_id uuid NOT NULL PRIMARY KEY,
-    repo_id uuid NOT NULL,
+    task_id uuid NOT NULL REFERENCES task(id),
+    repo_id uuid NOT NULL REFERENCES repository(id),
     file_masks varchar(64)[] NOT NULL
 );
 
 
-CREATE TABLE repository (
-    id uuid NOT NULL PRIMARY KEY
-);
-
-
 CREATE TABLE attempt (
-    attempt_id uuid NOT NULL PRIMARY KEY,
-    user_id uuid NOT NULL,
-    task_id uuid NOT NULL 
+    id uuid NOT NULL PRIMARY KEY,
+    user_id uuid NOT NULL REFERENCES users(id),
+    task_id uuid NOT NULL REFERENCES task(id) 
 );
 
 CREATE TABLE attempt_transitions (
-    attempt_id uuid NOT NULL,
+    attempt_id uuid NOT NULL REFERENCES attempt(id),
     state attempt_state NOT NULL,
-    transition_at timestamp,
+    transition_at timestamp NOT NULL,
     -- Can be used to save additional transition data (unpredictable for now)
     transition_data jsonb
 );
 
--- NOTE(nrydanov): Here we store git information if user used git repository
+-- NOTE(nrydanov): Here we store git information if users used git repository
 -- for submitting attempt
 CREATE TABLE attempt_git_info (
-    attempt_id uuid NOT NULL,
-    repo_id uuid NOT NULL,
+    attempt_id uuid NOT NULL REFERENCES attempt(id),
+    repo_id uuid NOT NULL REFERENCES repository(id),
     commit_hash bytea NOT NULL
 );
 
 CREATE TABLE attempt_classic_info (
-    attempt_id uuid NOT NULL,
-    uploaded_files uuid[] NOT NULL  
+    attempt_id uuid NOT NULL REFERENCES attempt(id),
+    uploaded_files uuid[] NOT NULL
 );
 
 CREATE TABLE stored_file (
     id uuid NOT NULL PRIMARY KEY,
     url varchar(64) NOT NULL,
-    uploaded_by uuid NOT NULL
+    uploaded_by uuid NOT NULL REFERENCES users(id)
 );
     
 
 CREATE TABLE task_block (
-    task_id uuid NOT NULL,
-    block_id uuid NOT NULL,
+    task_id uuid NOT NULL REFERENCES task(id),
+    block_id uuid NOT NULL REFERENCES block(id),
     index integer NOT NULL,
     hidden boolean NOT NULL
 );
 
 
 CREATE TABLE course_task (
-    task_id uuid NOT NULL,
-    course_id uuid NOT NULL
+    task_id uuid NOT NULL REFERENCES task(id),
+    course_id uuid NOT NULL REFERENCES course(id)
 );
 
 
-CREATE TABLE user_course (
-    course_id uuid NOT NULL,
-    user_id uuid NOT NULL
+CREATE TABLE users_course (
+    course_id uuid NOT NULL REFERENCES course(id),
+    user_id uuid NOT NULL REFERENCES users(id)
 );
 
 
-CREATE TABLE user_roles (
-    user_id uuid NOT NULL,
+CREATE TABLE users_role (
+    user_id uuid NOT NULL REFERENCES users(id),
     role user_role NOT NULL
 );
-
-CREATE TABLE specialty (
-    id uuid NOT NULL PRIMARY KEY,
-    name varchar(64)
-);
-
-CREATE TABLE faculty (
-    id uuid NOT NULL PRIMARY KEY,
-    name varchar(64)
-);
-
-
-ALTER TABLE users
-ADD CONSTRAINT users_specialty_fk1
-FOREIGN KEY (specialty_id)
-REFERENCES specialty (id);
-
-ALTER TABLE users
-ADD CONSTRAINT users_faculty_fk1
-FOREIGN KEY (faculty_id)
-REFERENCES faculty (id);
-
-ALTER TABLE user_roles
-ADD CONSTRAINT user_role_pk
-PRIMARY KEY (user_id, role);
-
-ALTER TABLE course
-ADD CONSTRAINT course_creator_fk1
-FOREIGN KEY (creator_id)
-REFERENCES users (id);
-
-ALTER TABLE user_course
-ADD CONSTRAINT user_course_pk
-PRIMARY KEY (user_id, course_id);
-
-ALTER TABLE user_course
-ADD CONSTRAINT user_course_fk1
-FOREIGN KEY (user_id)
-REFERENCES users (id);
-
-ALTER TABLE user_course
-ADD CONSTRAINT user_course_fk2
-FOREIGN KEY (course_id)
-REFERENCES course (id);
-
-
-ALTER TABLE course_user_group
-ADD CONSTRAINT course_user_group_pk
-PRIMARY KEY (user_id, course_id, group_id);
-
-
-ALTER TABLE course_user_group
-ADD CONSTRAINT course_user_group_fk1
-FOREIGN KEY (user_id)
-REFERENCES users (id);
-
-ALTER TABLE course_user_group
-ADD CONSTRAINT course_user_group_fk2
-FOREIGN KEY (course_id)
-REFERENCES course (id);
-
-ALTER TABLE course_user_group
-ADD CONSTRAINT course_user_group_fk3
-FOREIGN KEY (group_id)
-REFERENCES user_group (id);
-
-ALTER TABLE course_task
-ADD CONSTRAINT course_task_pk
-PRIMARY KEY (task_id, course_id);
-
-ALTER TABLE course_task
-ADD CONSTRAINT course_task_fk1
-FOREIGN KEY (course_id)
-REFERENCES course (id);
-
-ALTER TABLE course_task
-ADD CONSTRAINT course_task_fk2
-FOREIGN KEY (task_id)
-REFERENCES task (id);
-
-ALTER TABLE task_block
-ADD CONSTRAINT task_block_pk
-PRIMARY KEY (task_id, block_id);
-
-ALTER TABLE task_block
-ADD CONSTRAINT task_block_fk1
-FOREIGN KEY (task_id)
-REFERENCES task (id);
-
-ALTER TABLE task_block
-ADD CONSTRAINT task_block_fk2
-FOREIGN KEY (block_id)
-REFERENCES block (id);
-
-
-ALTER TABLE group_password
-ADD CONSTRAINT group_password_fk1
-FOREIGN KEY (group_id) 
-REFERENCES user_group (id);
-
-ALTER TABLE course_faculty
-ADD CONSTRAINT course_faculty_pk
-PRIMARY KEY (course_id, faculty_id);
-
-ALTER TABLE course_faculty
-ADD CONSTRAINT course_faculty_fk1
-FOREIGN KEY (course_id)
-REFERENCES course (id);
-
-ALTER TABLE course_faculty
-ADD CONSTRAINT course_faculty_fk2
-FOREIGN KEY (faculty_id)
-REFERENCES faculty (id);
-
-
-ALTER TABLE attempt
-ADD CONSTRAINT attempt_user_fk1
-FOREIGN KEY (user_id)
-REFERENCES users (id);
-
-ALTER TABLE attempt
-ADD CONSTRAINT attempt_task_fk2
-FOREIGN KEY (task_id)
-REFERENCES task (id);
-
-ALTER TABLE task_group_task
-ADD CONSTRAINT task_group_task_fk1
-FOREIGN KEY (task_id)
-REFERENCES task (id);
-
-
-ALTER TABLE task_git_info
-ADD CONSTRAINT task_git_info_fk1
-FOREIGN KEY (task_id)
-REFERENCES task (id);
-
-ALTER TABLE task_git_info
-ADD CONSTRAINT task_git_info_fk2
-FOREIGN KEY (repo_id)
-REFERENCES repository (id);
-
-ALTER TABLE stored_file
-ADD CONSTRAINT stored_file_user_fk
-FOREIGN KEY (uploaded_by)
-REFERENCES users (id);
